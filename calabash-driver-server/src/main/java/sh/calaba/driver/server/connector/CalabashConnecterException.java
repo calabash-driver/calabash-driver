1477f13335062d03f9487fbbed774a799e7e48ee
Dary, Dominik <ddary@ebay.com>
2012-09-19 20:01:03 +0200

    cleanup

--- calabash-driver-server/src/main/java/sh/calaba/driver/server/connector/CalabashConnecterException.java
+++ calabash-driver-server/src/main/java/sh/calaba/driver/server/connector/CalabashConnecterException.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2012 calabash-driver committers.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -19,15 +19,15 @@ package sh.calaba.driver.server.connector;
 public class CalabashConnecterException extends RuntimeException {
   private static final long serialVersionUID = 2953563072912935718L;
 
-  public CalabashConnecterException(String arg0, Throwable arg1) {
-    super(arg0, arg1);
+  public CalabashConnecterException(String msg, Throwable t) {
+    super(msg, t);
   }
 
-  public CalabashConnecterException(String arg0) {
-    super(arg0);
+  public CalabashConnecterException(String msg) {
+    super(msg);
   }
 
-  public CalabashConnecterException(Throwable arg0) {
-    super(arg0);
+  public CalabashConnecterException(Throwable t) {
+    super(t);
   }
 }
