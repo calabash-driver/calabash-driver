b2b6ce699b352503fbf8c00f086d81390cc71f3f
Dary, Dominik <ddary@ebay.com>
2012-09-19 20:00:08 +0200

    cleanup

--- calabash-driver-server/src/main/java/sh/calaba/driver/server/command/CalabashCommandHandler.java
+++ calabash-driver-server/src/main/java/sh/calaba/driver/server/command/CalabashCommandHandler.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2012 calabash-driver committers.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -31,14 +31,15 @@ public class CalabashCommandHandler extends BaseCommandHandler {
     JSONObject response =
         getCalabashProxy().redirectMessageToCalabashServer(getRequest().getPayload(), sessionId);
 
-    int responseStatusCode;
+    //important for the selenium grid
+    int webdriverResponseStatusCode;
     if (response.getBoolean("success")) {
-      responseStatusCode = 0;
+      webdriverResponseStatusCode = 0;
     } else {
-      responseStatusCode = 13;
+      webdriverResponseStatusCode = 13;
     }
 
-    return new WebDriverLikeResponse(sessionId, responseStatusCode, response);
+    return new WebDriverLikeResponse(sessionId, webdriverResponseStatusCode, response);
   }
 
 }
